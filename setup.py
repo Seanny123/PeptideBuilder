85a499fbc0ff251dbd4a922102120d68f35dfa18
Sean Aubin <sean@proteinqure.com>
2020-03-26 20:20:54 -0400

    autoformatted with black

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ with open("PeptideBuilder/__init__.py", "r") as f:
     init = f.readlines()
 
 for line in init:
-    if '__version__' in line:
+    if "__version__" in line:
         __version__ = line.split("'")[-2]
 
-setup(name = 'PeptideBuilder', 
-    version = __version__, 
-    author = 'Matthew Z. Tien', 
-    author_email = 'Matthew.Tien89@gmail.com', 
-    description = 'Create peptide PDB files with specified geometry',
+setup(
+    name="PeptideBuilder",
+    version=__version__,
+    author="Matthew Z. Tien",
+    author_email="Matthew.Tien89@gmail.com",
+    description="Create peptide PDB files with specified geometry",
     long_description=long_description,
-    long_description_content_type='ext/markdown',
-    url = 'https://github.com/mtien/PeptideBuilder',
-    download_url = 'https://github.com/mtien/PeptideBuilder/releases',
-    platforms = 'Tested on Mac OS X and Windows 10',
-    packages = ['PeptideBuilder'],
-    install_requires=['Biopython']
+    long_description_content_type="ext/markdown",
+    url="https://github.com/mtien/PeptideBuilder",
+    download_url="https://github.com/mtien/PeptideBuilder/releases",
+    platforms="Tested on Mac OS X and Windows 10",
+    packages=["PeptideBuilder"],
+    install_requires=["Biopython"],
 )
--- unitTests.py
+++ unitTests.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-'''
+"""
 This script builds a variety of structures using all the major functionality in PeptideBuilder. Compare the generated structures to the provided reference structures to see if everything is correct.
-'''
+"""
 
 from __future__ import print_function
 from PeptideBuilder import Geometry
@@ -11,11 +11,11 @@ import Bio.PDB
 
 
 # Build a peptide containing all 20 amino acids
-structure = PeptideBuilder.initialize_res('A')
+structure = PeptideBuilder.initialize_res("A")
 
 for aa in "CDEFGHIKLMNPQRSTVWY":
     structure = PeptideBuilder.add_residue(structure, aa)
-    
+
 out = Bio.PDB.PDBIO()
 out.set_structure(structure)
 out.save("test1.pdb")
@@ -23,7 +23,7 @@ out.save("test1.pdb")
 # Build a helix containing all 20 amino acids, with slowly varying backbone angles
 phi = -60
 psi_im1 = -40
-geo = Geometry.geometry('A')
+geo = Geometry.geometry("A")
 geo.phi = phi
 geo.psi_im1 = psi_im1
 structure = PeptideBuilder.initialize_res(geo)
@@ -35,12 +35,12 @@ for aa in "CDEFGHIKLMNPQRSTVWY":
     geo.phi = phi
     geo.psi_im1 = psi_im1
     structure = PeptideBuilder.add_residue(structure, geo)
-    
+
 out.set_structure(structure)
 out.save("test2.pdb")
 
 # Print out all geometries
-outfile = open("test3.txt", 'w')
+outfile = open("test3.txt", "w")
 for aa in "ACDEFGHIKLMNPQRSTVWY":
     print(outfile, Geometry.geometry(aa))
 
@@ -67,7 +67,7 @@ out.save("test4.pdb")
 geos = []
 for aa in "ACDEFGHIKLMNPQRSTVWY":
     geos.append(Geometry.geometry(aa))
-structure = PeptideBuilder.make_structure_from_geos(geos)  
+structure = PeptideBuilder.make_structure_from_geos(geos)
 out.set_structure(structure)
 out.save("test5.pdb")
 
@@ -78,11 +78,12 @@ out.save("test5.pdb")
 phi_list = []
 psi_im1_list = []
 
-for i in range(1,20):
-    phi_list.append(-60+i)
-    psi_im1_list.append(-40-i)
-structure = PeptideBuilder.make_structure("ACDEFGHIKLMNPQRSTVWY",\
-                                                phi_list, psi_im1_list)
+for i in range(1, 20):
+    phi_list.append(-60 + i)
+    psi_im1_list.append(-40 - i)
+structure = PeptideBuilder.make_structure(
+    "ACDEFGHIKLMNPQRSTVWY", phi_list, psi_im1_list
+)
 out.set_structure(structure)
 out.save("test6.pdb")
 
@@ -94,17 +95,16 @@ phi_list = []
 psi_im1_list = []
 omega_list = []
 
-for i in range(1,20):
-    phi_list.append(-60+i)
-    psi_im1_list.append(-40-i)
+for i in range(1, 20):
+    phi_list.append(-60 + i)
+    psi_im1_list.append(-40 - i)
     omega_list.append(180)
-    
-for i in range(9,19):
+
+for i in range(9, 19):
     omega_list[i] = -178
-    
-structure = PeptideBuilder.make_structure("ACDEFGHIKLMNPQRSTVWY",\
-                                                phi_list, psi_im1_list,\
-                                                omega_list)
+
+structure = PeptideBuilder.make_structure(
+    "ACDEFGHIKLMNPQRSTVWY", phi_list, psi_im1_list, omega_list
+)
 out.set_structure(structure)
 out.save("test7.pdb")
-
